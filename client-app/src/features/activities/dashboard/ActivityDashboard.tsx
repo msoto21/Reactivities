7f89e51702ef27dcd7ca51dbd56e09b9e0d9073c
msoto21 <84418999+msoto21@users.noreply.github.com>
2021-09-16 08:59:00 -0700

    End of section 9

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -3,6 +3,7 @@ import React, { useEffect } from 'react';
 import { Grid } from 'semantic-ui-react';
 import LoadingComponent from '../../../app/layout/LoadingComponent';
 import { useStore } from '../../../app/stores/store';
+import ActivityFilters from './ActivityFilters';
 import ActivityList from './ActivityList';
 
 export default observer (function ActivityDashboard() {  // ({activities, deleteActivity, submitting}: Props) { // (props: Props) is similar to ({activities}: Props)
@@ -22,7 +23,7 @@ export default observer (function ActivityDashboard() {  // ({activities, delete
                <ActivityList />
             </Grid.Column>
             <Grid.Column width='6'>
-                <h2>Activity filters coming soon</h2>
+                <ActivityFilters />
             </Grid.Column>
         </Grid>
     )
