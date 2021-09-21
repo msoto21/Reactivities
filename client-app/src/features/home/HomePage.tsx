0ae1d26648a61397539d0121b4a479c91f424d4d
msoto21 <84418999+msoto21@users.noreply.github.com>
2021-09-21 15:24:36 -0700

    End of section 10

--- client-app/src/features/home/HomePage.tsx
+++ client-app/src/features/home/HomePage.tsx
@@ -5,7 +5,7 @@ import { Container, Header, Segment, Image, Button } from "semantic-ui-react";
 export default function HomePage() {
     return (
         <Segment  inverted textAlign='center' vertical className='masthead'>
-            <Container text>
+            <Container text>            
                 <Header as='h1' inverted>
                     <Image size='massive' src='/assets/logo.png' alt='logo' style={{marginBottom: 12}} />
                     Reactivities
